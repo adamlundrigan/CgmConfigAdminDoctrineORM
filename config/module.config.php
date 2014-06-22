8c3e5f79a9a03b68289b557937dbd61ef0f69242
Adam Lundrigan <adam@lundrigan.ca>
2014-06-22 13:39:19 -0230

    Add CgmConfigAdminDoctrineORM\Entity to Doctrine driver chain

--- config/module.config.php
+++ config/module.config.php
@@ -6,10 +6,15 @@ return array(
                 'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
                 'paths' => __DIR__ . '/xml/cgmconfigadmin'
             ),
+            'cgmconfigadmindoctrineorm_entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
+                'paths' => __DIR__ . '/xml/cgmconfigadmindoctrineorm'
+            ),
 
             'orm_default' => array(
                 'drivers' => array(
-                    'CgmConfigAdmin\Entity'  => 'cgmconfigadmin_entity'
+                    'CgmConfigAdmin\Entity'  => 'cgmconfigadmin_entity',
+                    'CgmConfigAdminDoctrineORM\Entity'  => 'cgmconfigadmindoctrineorm_entity',
                 )
             )
         )
